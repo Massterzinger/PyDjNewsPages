8ce3d3e694f575e0ac78ab0e332df3bd508f54cb
Richard <wromankosawp@gmail.com>
2017-07-28 13:49:49 +0300

    added news app

---
+++ djpostsenv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/florian/01D1F32F6143C9A0/MyMade/Python/DjangoProj/djposts/djpostsenv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
