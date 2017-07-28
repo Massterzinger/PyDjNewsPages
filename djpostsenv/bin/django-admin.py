468771b7e6b99dcd1909ed2dc1201501abbdafdc
Richard <wromankosawp@gmail.com>
2017-07-28 13:58:02 +0300

    added news app, without env

--- djpostsenv/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/florian/01D1F32F6143C9A0/MyMade/Python/DjangoProj/djposts/djpostsenv/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
