ed0c236f00d9f032c6df724ff07edb9f351c1f12
Richard <wromankosawp@gmail.com>
2017-07-29 21:18:18 +0300

    Added pagination

--- news/views.py
+++ news/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from django.http import HttpResponse
 from news.models import NewsPost
 
@@ -8,7 +9,16 @@ def archive(request):
     obs = NewsPost.objects.all()
     for i in obs:
         i.body = i.body[:200]+'...'
-    return HttpResponse(render(request, "archive.html", {'posts':obs} ))
+    page = request.GET.get('page', 1)
+
+    paginator = Paginator(obs, 10)
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        posts = paginator.page(1)
+    except EmptyPage:
+        posts = paginator.page(paginator.num_pages)
+    return HttpResponse(render(request, "archive.html", {'posts':posts} ))
 
 def newsdetail(request, NewsPost_id):
     obs = NewsPost.objects.filter(id=NewsPost_id)
